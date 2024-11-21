8b6a4311e947e53a4e58437145a4374fcd02b2d7
donke007 <donke007@outlook.com>
2024-11-21 12:54:43 +0800

    feat: add localforage

--- js/biliScript.js
+++ js/biliScript.js
@@ -4,25 +4,20 @@
 // @version      1.0.0
 // @description  样式优化。
 // @author       donke
-// @icon         
+// @icon
 // @match        *://*.bilibili.com/*
 
 // ==/UserScript==
 
-
 (function () {
   'use strict';
-  var script1 = document.createElement('script')
-  script1.setAttribute(
-    'src',
-    'https://code.jquery.com/jquery-3.7.1.slim.min.js'
-  );
-  var script2 = document.createElement('script')
-  script1.setAttribute(
+  var script1 = document.createElement('script');
+  script1.setAttribute('src', 'https://code.jquery.com/jquery-3.7.1.min.js');
+  document.head.appendChild(script1);
+  var script2 = document.createElement('script');
+  script2.setAttribute(
     'src',
     'https://donke007.github.io/webSitStyleHub/js/bili.js'
   );
-  document.head.appendChild(script1);
   document.head.appendChild(script2);
-  
 })();
