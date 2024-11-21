c9452ddf6144a71b2c6824ddfd93cc4cac1880a2
donke007 <donke007@outlook.com>
2024-11-21 12:13:38 +0800

    feat: add bili

---
+++ js/biliScript.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         bilibili样式优化
+// @namespace    bili_style
+// @version      1.0.0
+// @description  样式优化。
+// @author       donke
+// @icon         
+// @match        *://*.bilibili.com/*
+
+// ==/UserScript==
+
+
+(function () {
+  'use strict';
+  var $ = require('')
+  
+})();
